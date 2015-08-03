cc6512521b8b7314932b847c96b61c52ded89afd
magnolo <office@manfredwalder.at>
2015-08-03 09:30:48 +0200

    Correct Menü Viewing

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,7 @@ module.exports = function(grunt) {
                 importPath: '<%= yeoman.app %>/bower_components',
                 httpImagesPath: '/images',
                 httpGeneratedImagesPath: '/images/generated',
-                httpFontsPath: '/styles/fonts',
+                httpFontsPath: '/2.3/styles/fonts',
                 relativeAssets: false,
                 assetCacheBuster: false,
                 raw: 'Sass::Script::Number.precision = 10\n'
