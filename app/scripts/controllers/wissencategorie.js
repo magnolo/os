68d52c98be0ed904a004adeef61034a75569776b
magnolo <office@manfredwalder.at>
2015-12-03 09:17:04 +0100

    replacing old editor and removing all dependencies

--- app/scripts/controllers/wissencategorie.js
+++ app/scripts/controllers/wissencategorie.js
@@ -340,59 +340,7 @@ angular.module('osApp')
 			$scope.saveArticle();
 		};
 		$scope.froalaOptions = {};
-		$scope.ckeOptions = {
-			language: 'de',
-			//allowedContent: true,
-			entities: false,
-			skin: 'flat',
-			toolbarGroups: [{
-				name: 'clipboard',
-				groups: ['clipboard', 'undo']
-			}, {
-				name: 'links',
-				groups: ['links']
-			}, {
-				name: 'insert',
-				groups: ['insert']
-			}, {
-				name: 'forms',
-				groups: ['forms']
-			}, {
-				name: 'document',
-				groups: ['mode', 'document', 'doctools']
-			}, {
-				name: 'others',
-				groups: ['others']
-			}, {
-				name: 'basicstyles',
-				groups: ['basicstyles', 'cleanup']
-			}, {
-				name: 'paragraph',
-				groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']
-			}, {
-				name: 'styles',
-				groups: ['styles']
-			}, {
-				name: 'tools',
-				groups: ['tools']
-			}, {
-				name: 'editing',
-				groups: ['find', 'selection', 'spellchecker', 'editing']
-			}, {
-				name: 'colors',
-				groups: ['colors']
-			}, {
-				name: 'about',
-				groups: ['about']
-			}],
-			removeButtons: 'Underline,Subscript,Superscript,About,Styles,Table,HorizontalRule,SpecialChar,Source,Anchor',
-			removePlugins: 'language,tableresize,tabletools,scayt,menubutton,contextmenu',
-			resize_maxwidth: 460,
-			resize_minwidth: 460,
-			width: 460,
-			filebrowserImageUploadUrl: 'http://www.openscience.or.at/assets/ajax/uploadContentImage.php?id=content&cat=articles',
-			extraPlugins: 'youtube,uploadimage,image2,notification'
-		};
+
 		$scope.getText = function () {
 			//console.log($scope.froalaOptions.froala("showMediaManager"));
 		};
